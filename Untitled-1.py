1fbd29f7714e5f7cf929ed5da987acc6adaadb35
Isaacberserk <losa8715@gmail.com>
2024-10-10 15:37:54 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
